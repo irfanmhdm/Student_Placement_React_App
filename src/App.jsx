ebeefa8f92fde7d5acabca9d39418c1b86e9b2a9
Muhammed Irfan S <irfanmhdm123@gmail.com>
2026-07-11 20:54:29 +0530

    Search Completed

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import Addstudent from './components/Addstud'
+import Search from './components/Search'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,6 +13,7 @@ function App() {
     <>
 
     <Addstudent/>
+    <Search/>
       
     </>
   )
